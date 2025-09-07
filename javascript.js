983099522bf148d6c53006ded23a78ade92a7718
rohithkumaro <rohith.kumar@yellow.ai>
2025-09-07 10:51:17 +0530

    added swaping feature

--- javascript.js
+++ javascript.js
@@ -1,15 +1,17 @@
 const dropdowns = document.querySelectorAll(".CountryDropdown");
 let frominputplaceholder = document.querySelector(".frominputplaceholder");
 let toinputplaceholder = document.querySelector(".toinputplaceholder");
+let convertIcon = document.querySelector(".fa-right-left");
+let Flags = document.querySelectorAll(".Flag");
+
 async function getlatestRate(from, to) {
   const url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${from}.min.json`;
-  const data = await fetch(url).then(res => res.json());
+  const data = await fetch(url).then((res) => res.json());
   if (!data[from][to]) {
     throw new Error(`Currency '${to}' not found in response`);
   }
   return data[from][to];
 }
-
 
 let selects = document.querySelectorAll("select");
 let fromOption = selects[0];
@@ -42,8 +44,8 @@ const updateFlag = (element) => {
   let Newsrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
   let img = element.parentElement.querySelector("img");
   img.src = Newsrc;
-  frominputplaceholder.value = ""; 
-  toinputplaceholder.value = ""; 
+  frominputplaceholder.value = "";
+  toinputplaceholder.value = "";
 };
 
 const log = (Print) => {
@@ -52,9 +54,9 @@ const log = (Print) => {
 
 frominputplaceholder.addEventListener("input", async () => {
   let inputValue = frominputplaceholder.value;
-  
-//   log(fromOption.value);  
-//   log(toOption.value);
+
+  //   log(fromOption.value);
+  //   log(toOption.value);
 
   let lowerFrom = fromOption.value.toLowerCase();
   let lowerTo = toOption.value.toLowerCase();
@@ -63,9 +65,52 @@ frominputplaceholder.addEventListener("input", async () => {
     const rate = await getlatestRate(lowerFrom, lowerTo);
     // console.log(rate);
     toinputplaceholder.value = inputValue * rate;
-  } catch(err) {
+  } catch (err) {
     console.error(err.message);
   }
+});
 
-  
+toinputplaceholder.addEventListener("input", async () => {
+  let inputValue = toinputplaceholder.value;
+
+  //   log(fromOption.value);
+  //   log(toOption.value);
+
+  let lowerFrom = toOption.value.toLowerCase();
+  let lowerTo = fromOption.value.toLowerCase();
+
+  try {
+    const rate = await getlatestRate(lowerFrom, lowerTo);
+    // console.log(rate);
+    frominputplaceholder.value = inputValue * rate;
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+let rotation = 0;
+
+convertIcon.addEventListener("click", async () => {
+  rotation += 180;
+  convertIcon.style.transform = `rotate(${rotation}deg)`;
+
+  let tempOptionValue = fromOption.value;
+  fromOption.value = toOption.value;
+  toOption.value = tempOptionValue;
+
+  log(fromOption.value);
+  log(toOption.value);
+
+  let FirstCountryCode = fromOption.value.toUpperCase();
+  let SecondCountryCode = toOption.value.toUpperCase();
+
+  Flags[0].src = `https://flagsapi.com/${countryList[FirstCountryCode]}/flat/64.png`;
+
+  Flags[1].src = `https://flagsapi.com/${countryList[SecondCountryCode]}/flat/64.png`;
+
+  lowerFrom = fromOption.value.toLowerCase();
+  lowerTo = toOption.value.toLowerCase();
+
+  const rate = await getlatestRate(lowerFrom, lowerTo);
+  toinputplaceholder.value = frominputplaceholder.value * rate;
 });
